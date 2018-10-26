50a1ef7fc5b8ca9c5743857adf0d3454cd097719
AD-ONE\pawan <pawan.mandhan@soprasteria.com>
2018-10-26 12:53:17 +0530

    removed basescanPackages

--- src/main/java/com/docmarker/DocMarkerServiceApplication.java
+++ src/main/java/com/docmarker/DocMarkerServiceApplication.java
@@ -4,8 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jmx.JmxAutoConfiguration;
 
-//@ComponentScan(basePackages = "com.docmarker.")
-@SpringBootApplication(scanBasePackages = "com.docmarker.repository", exclude = JmxAutoConfiguration.class)
+@SpringBootApplication( exclude = JmxAutoConfiguration.class)
 public class DocMarkerServiceApplication {
 
 	public static void main(String[] args) {
