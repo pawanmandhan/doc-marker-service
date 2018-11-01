cad2fd002dda1fef1345688290036e4b0e39053a
AD-ONE\pawan <pawan.mandhan@soprasteria.com>
2018-11-01 14:23:09 +0530

    some changes

--- src/main/java/com/docmarker/config/server/ResourceServerConfiguration.java
+++ src/main/java/com/docmarker/config/server/ResourceServerConfiguration.java
@@ -3,7 +3,6 @@ package com.docmarker.config.server;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
