4ed29c93594672142a018ef9de1672b9c89628d8
AD-ONE\pawan <pawan.mandhan@soprasteria.com>
2018-11-15 17:32:42 +0530

    formatting of service

--- src/main/java/com/docmarker/service/IUserService.java
+++ src/main/java/com/docmarker/service/IUserService.java
@@ -6,10 +6,15 @@ import com.docmarker.model.VerificationToken;
 import com.docmarker.model.security.User;
 
 public interface IUserService {
-    User registerUser(UserDto accountDto) throws EmailAlreadyUsedException;
-    void createVerificationToken(User user, String token);
-    VerificationToken getVerificationToken(String verificationToken);
-    void saveRegisteredUser(User user);
-    VerificationToken generateNewVerificationToken(String token);
-    User getUser(String verificationToken);
+	User registerUser(UserDto accountDto) throws EmailAlreadyUsedException;
+
+	void createVerificationToken(User user, String token);
+
+	VerificationToken getVerificationToken(String verificationToken);
+
+	void saveRegisteredUser(User user);
+
+	VerificationToken generateNewVerificationToken(String token);
+
+	User getUser(String verificationToken);
 }
